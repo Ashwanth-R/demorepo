b22197e473ba5ca01fae7551079c7d3776638440
Ashwanth <96193365+Ashwanth-R@users.noreply.github.com>
2025-06-25 15:30:45 +0530

    Update login.py added logger part

--- login.py
+++ login.py
@@ -10,7 +10,7 @@ def login_system():
     password = input("Enter password: ")
 
     if username == correct_username and password == correct_password:
-        print("Login successful! Welcome.......")
+        print("Login successful! Welcome.......") 
     else:
         print("Invalid username or password. Please try again!!!!!!!")
 
