f0854143c663fe8b50b2e675b8418d1a306d873e
Ashwanth <96193365+Ashwanth-R@users.noreply.github.com>
2025-06-27 11:23:47 +0530

    Update sampleML.py

--- sampleML.py
+++ sampleML.py
@@ -18,7 +18,7 @@ model = LinearRegression()
 # Train the model using the training data
 model.fit(X_train, y_train)
 
-# Make predictions on the test data
+# Make predictions on the test data 
 y_pred = model.predict(X_test)
 
 # Evaluate the model
